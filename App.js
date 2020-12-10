870661302d0c260e5e06d74d4e536e54e3094d4e
kuma88 <kumarasond@gmail.com>
2020-12-11 03:22:47 +0800

    second commit no changes

---
+++ App.js
@@ -0,0 +1,79 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+
+// const db = SQLite.openDatabase("notes.db");
+
+function NotesScreen({ navigation }) {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote}>
+          <Ionicons
+            name="ios-create-outline"
+            size={30}
+            color="black"
+            style={{
+              color: "#f55",
+              marginRight: 10,
+            }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  function addNote() {
+    alert("Adding a new note!");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Notes"
+          component={NotesScreen}
+          options={{
+            title: "Notes, a Todo App",
+            headerStyle: {
+              backgroundColor: "yellow",
+              height: 100,
+              shadowColor: "black",
+              shadowOpacity: 0.2,
+              shadowRadius: 5,
+            },
+            headerTintColor: "#f55",
+            headerTitleStyle: {
+              fontSize: 24,
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
